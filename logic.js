d215601bdc8add89c921269fbee6c68824ef7a21
Jeriko <jcarrico1985@live.com>
2018-01-26 15:00:14 -0600

    created logic for konami code activation. placed konami text on bottom right of page

--- logic.js
+++ logic.js
@@ -2,8 +2,30 @@
 
 (() => {
 
+  const konamiCode = [38,38,40,40,37,39,37,39];
+
   const welcome = () => {
 
-  }
+  };
+
+  const konamiCodeMatcher = () => {
+    let konamiMatch = [];
+    let i = 0;
+    document.addEventListener('keydown', function(e) {
+      if(e.which === konamiCode[i]) {
+        konamiMatch.push(e.which);
+        i++;
+        console.log(konamiMatch);
+        if(konamiCode.every((e, i) => e === konamiMatch[i])) {
+          console.log('woo');
+        }
+      } else {
+        i = 0;
+        konamiMatch = [];
+      }
+    });
+  };
+
+  konamiCodeMatcher();
 
 })();
