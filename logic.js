702d4e798db9108a08c8c6caac17544b34fae724
Jeriko <jcarrico1985@live.com>
2018-04-06 16:17:23 -0500

    updated konamiCodeMatcher function to perform actions on completion of code match. fixed and renamed hexgenerator function to randomHex. Completed secret() function

--- logic.js
+++ logic.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (() => {
+  let cakeBody = document.querySelector('#cake-body');
+  let happyBdayTxt = document.querySelector('#happy-bday-text');
 
   const konamiCode = [38,38,40,40,37,39,37,39,66,65];
 
@@ -9,43 +11,53 @@
   };
 
   const secret = () => {
-
+    cakeBody.classList.toggle('slide-down');
+    happyBdayTxt.classList.toggle('slide-down-h1');
   };
 
   const konamiCodeMatcher = () => {
     let konamiMatch = [];
     let i = 0;
-    document.addEventListener('keydown', function(e) {
+    document.addEventListener('keydown', e => {
       if(e.which === konamiCode[i]) {
         konamiMatch.push(e.which);
         i++;
-        console.log(konamiMatch);
-        if(konamiCode.every((e, i) => e === konamiMatch[i])) {
-          hexGenerator();
+        if(konamiCode.every((el, i) => el === konamiMatch[i])) {
+          setTimeout(() => {
+            document.querySelector('#audio').play();
+          }, 3000);
+          setInterval(() => {
+            document.body.style.backgroundColor = randomHex();
+          }, 250);
           secret();
+          cakeBody.style.top = '50px';
+          happyBdayTxt.style.top = '0';
         }
-      } else {
+      } else { //if the keycode that was pressed doesn't match the konami code at index i, reset the player's array and reset the iterator to 0 (to look at the beginning of the konami array again)
         i = 0;
         konamiMatch = [];
       }
     });
   };
 
-  const hexGenerator = () => {
-    let hex = [];
-    let i=0;
-    do {
-      hex.push(randomNum(48,9));
-      i++;
-    } while (i<6);
-    hex.join('');
-    console.log(hex.join(''));
-    return hex;
-  };
-
   const randomNum = (start, toNum) => {
     return Math.floor((Math.random() * toNum) + start);
   };
+
+  const randomHex = () => {
+    let hex = ['#'];
+    let hexChars = ['a', 'b', 'c', 'd', 'e', 'f'];
+    for(let i=0; i<6; i++) {
+      let flag = Math.round(Math.random());
+      let random;
+      flag ? random = randomNum(0,6) : random = hexChars[randomNum(0, hexChars.length-1)];
+      hex.push(random);
+    }
+    hex = hex.join('');
+    return hex;
+  };
+
+  console.log(randomHex());
 
   konamiCodeMatcher();
 
